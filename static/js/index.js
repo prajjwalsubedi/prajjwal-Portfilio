19fc5b246698154585a56b8a1ae5bb80145a4473
prajjwalsubedi <prajjwalsubedi95@gmail.com>
2022-07-21 20:03:59 +1000

    1

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
 function navBarFunction() {
-    document.getElementById('menu-mbl').style.display='block';
-    document.getElementById('nav-close').style.display='flex';    
-    document.getElementById('mbl-menu-bar').style.display='none';    
-  }
+  document.getElementById('menu-mbl').style.display='flex';
+  document.getElementById('nav-close').style.display='flex';    
+  document.getElementById('mbl-menu-bar').style.display='none';    
+}
 function navBarCloseFunction() {
-    document.getElementById('menu-mbl').style.display='none';
-    document.getElementById('nav-close').style.display='none';    
-    document.getElementById('mbl-menu-bar').style.display='block';    
-  }
+  document.getElementById('menu-mbl').style.display='none';
+  document.getElementById('nav-close').style.display='none';    
+  document.getElementById('mbl-menu-bar').style.display='contents';    
+}
